168b4ac187d3e81769e3c90e419f55e0f4409a43
TanmayMene <tanmaymene2004@gmail.com>
2024-01-02 22:52:11 +0530

    2 Jan 2024

---
+++ 2 Jan/Majority_Element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int cnt = 0;
+        int element = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(cnt == 0) {
+                element = nums[i];
+                cnt = 1;
+            }
+            else {
+                if(nums[i] == element) {
+                    cnt++;
+                }
+                else {
+                    cnt--;
+                }
+            }
+        }
+        return element;
+    }
+};
