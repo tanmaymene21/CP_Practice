2b771ecbea49fd127a8aba6c36d507de90253e8e
TanmayMene <tanmaymene2004@gmail.com>
2024-01-02 23:11:06 +0530

    2 Jan

---
+++ 2 Jan/Interesting_Subarray.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>> t;
+	while(t--) {
+	    int n;
+	    cin>> n;
+	    int arr[n];
+	    for(int i = 0; i < n; i++) cin>> arr[i];
+	    
+	    sort(arr, arr+n);
+	    long long minIntVal = arr[0], maxIntVal = arr[n-1];
+	    long long maxAns = max(minIntVal * minIntVal, maxIntVal * maxIntVal);
+	    long long minAns = min(minIntVal * minIntVal, min(maxIntVal * maxIntVal, maxIntVal * minIntVal));
+	    
+	    cout<< minAns << " " << maxAns << endl;
+	}
+
+}
